78908c72730d181ca004de5698fb44ec72f74e82
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-08-17 14:01:15 +0200

    Fix empty touch overlay composable content

--- app/src/main/kotlin/com/metrolist/music/ui/screens/BrowseScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/BrowseScreen.kt
@@ -182,7 +182,7 @@ fun BrowseScreen(
                                             onLongPress = { onItemLongClick() },
                                         )
                                     },
-                        )
+                        ) {}
                     }
                 }
             }
