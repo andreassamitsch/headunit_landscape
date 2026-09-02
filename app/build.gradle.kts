418bfc00d658aca144d1bceaf71f8d084e6863e1
asam <47712152+andreassamitsch@users.noreply.github.com>
2026-09-02 16:57:53 +0200

    build: release dudu7 13.7.81

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 1370089
-        versionName = "13.7.80"
+        versionCode = 1370090
+        versionName = "13.7.81"
         resValue("string", "app_name", appNameOverride ?: "Metrolist dudu7")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
